e1939dc75b1a7d8489d775846e19c1bac550665d
gizemcoban <gizem.coban.r@gmail.com>
2025-01-12 00:16:30 +0300

    fs.readFile and fs.writeFile functions

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try{
+        const data = await fs.readFile(PATH_DB,'utf-8');
+        console.log("File content",data);
+    }catch(error){
+        console.error("File reading error",error);
+    }
+};
+
+
+/*
+src/utils/readContacts.js dosyasında readContacts adında bir yardımcı fonksiyon tanımlayın. 
+Bu fonksiyon, src/db/db.json dosyasının içeriğini okumalı ve geri döndürmelidir.
+ Fonksiyon, verileri doğru bir şekilde işleyerek dosyadan verilerin doğru bir şekilde okunmasını sağlamalıdır.
+
+*/
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try{
+        const data = await fs.writeFile(PATH_DB,updatedContacts,'utf-8');
+        console.log("my data:",data);
+    }catch(error){
+        console.log(error);
+    }
+};
+
+
+/*
+src/utils/writeContacts.js dosyasında writeContacts adında bir yardımcı fonksiyon tanımlayın. 
+Bu fonksiyon, src/db/db.json dosyasına veri yazmalıdır.
+ Fonksiyon, verileri parametre olarak doğru bir şekilde almalı ve dosyaya verilerin doğru bir şekilde yazılmasını sağlamalıdır.
+
+*/
