8175afaaf1947f7e3230c3dddae6748c1f15a41d
gizemcoban <gizem.coban.r@gmail.com>
2025-01-13 12:47:33 +0300

    addOneContact.js completed

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,12 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs';
-import { createFakeContact } from "../utils/createFakeContact";
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { getAllContacts } from './getAllContacts.js';
 
 
 export const addOneContact = async () => {
+    let contacts = await getAllContacts();
     let contact = createFakeContact();
-    let contactString = JSON.stringify(contact)+'\n';
-    await fs.promises.appendFile(PATH_DB,contactString,'utf-8');
+    contacts.push(contact);
+    let contactString = JSON.stringify(contacts, null ,2)+'\n';
+    await fs.promises.writeFile(PATH_DB,contactString,'utf-8');
 };
 
 addOneContact();
